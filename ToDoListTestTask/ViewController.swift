5ae6c3ec407675821de19dbf5933e8d1fca0a0e7
korotkovaleksandr <81365352+AleksandrShlepkin@users.noreply.github.com>
2024-09-17 11:14:58 +0300

    Initial Commit

---
+++ ToDoListTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListTestTask
+//
+//  Created by Александр Коротков on 17.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
